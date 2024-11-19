8ffc40cda4c62a96eceb2c1c055e1b45af0d1e5c
Vyacheslav Tokarev <vyacheslav.tokarev@gmail.com>
2024-11-19 18:18:00 +0100

    Double to double

--- src/main/java/com/lt/spring/labs/controllers/TemperatureController.java
+++ src/main/java/com/lt/spring/labs/controllers/TemperatureController.java
@@ -13,24 +13,24 @@ public class TemperatureController {
     @GetMapping
     ResponseEntity<Integer> getTemperature() {
         double rand = Math.random() * 100;
-        int temp = scaled(rand, -15.0, 60, 0, 100).intValue();
+        int temp = (int) scaled(rand, -15.0, 60, 0, 100);
         return ResponseEntity.ok(temp);
     }
 
     @GetMapping("cel-to-fah/{celsius}")
     public ResponseEntity<Integer> convertCelsiusToFahrenheit(@PathVariable double celsius) {
-        double result = (celsius * 9/5)+32;
+        double result = (celsius * 9 / 5) + 32;
         return ResponseEntity.ok((int) result);
     }
 
     @GetMapping("fah-to-cel/{fahrenheit}")
     public ResponseEntity<Integer> convertFahrenheitToCelsius(@PathVariable double fahrenheit) {
-        double result = (fahrenheit - 32) * 5/9;
+        double result = (fahrenheit - 32) * 5 / 9;
         return ResponseEntity.ok((int) result);
     }
 
 
-    private Double scaled(double num, double scaleToMin, double scaleToMax, double currentMin, double currentMax) {
+    private double scaled(double num, double scaleToMin, double scaleToMax, double currentMin, double currentMax) {
         return (scaleToMax - scaleToMin) * (num - currentMin) / (currentMax - currentMin) + scaleToMin;
     }
 
